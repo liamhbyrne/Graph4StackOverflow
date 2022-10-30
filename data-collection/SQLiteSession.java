7cfab4fa1d90e2ec31faf0664cb499b805f7c32a
Liam Byrne <liambyrneh@gmail.com>
2022-10-30 21:56:22 +0000

    paragraph embeddings and basic regex code features pulling

--- data-collection/SQLiteSession.java
+++ data-collection/SQLiteSession.java
@@ -3,18 +3,26 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.config.Configurator;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.sql.*;
 
 public class SQLiteSession {
     private final Logger logger = LogManager.getLogger(SQLiteSession.class);
     private Connection conn;
+
+    private PreparedStatement ps;
+
+    private int maxBatchSize;
+    private int currentBatchsize = 0;
+
     public SQLiteSession(String url) {
-        Configurator.setLevel(logger.getName(), Level.INFO);
+        Configurator.setLevel(logger.getName(), Level.WARN);
         try {
             // create a connection to the database
             conn = DriverManager.getConnection(url);
+            conn.setAutoCommit(false);
             logger.info("Connection to SQLite has been established.");
         } catch (SQLException e) {
             logger.error(e.getMessage());
@@ -31,10 +39,43 @@ public class SQLiteSession {
         }
     }
 
-    public static void main(String[] args) {
+    public void createTables() throws SQLException, IOException {
+        /**
+         * Create tables using the `create.sql` file in the directory
+         */
+        Statement stmt = conn.createStatement();
+        String query = Files.readString(Path.of("create.sql"));
+        stmt.executeUpdate(query);
+        conn.commit();
+    }
+
+    public void setPreparedStatement(String baseQuery, int batchSize) throws SQLException {
+        this.ps = conn.prepareStatement(baseQuery);
+        this.maxBatchSize = batchSize;
+    }
+
+    public PreparedStatement getPreparedStatement() {
+        return ps;
+    }
+
+    public void processBatch() throws SQLException {
+        ps.addBatch();
+        if (++currentBatchsize > maxBatchSize) {
+            ps.executeBatch();
+            currentBatchsize = 0;
+            logger.info("Executed batch insert.");
+        }
+    }
+
+    public void commitChanges() throws SQLException {
+        conn.commit();
+    }
+
+    public static void main(String[] args) throws SQLException, IOException {
         SQLiteSession s = new SQLiteSession(
-        "jdbc:sqlite:..\\stackoverflow.db"
+                "jdbc:sqlite:..\\stackoverflow.db"
         );
+        s.createTables();
         s.endSession();
     }
 }
---
+++ data-collection/create.sql
@@ -0,0 +1,78 @@
+CREATE TABLE IF NOT EXISTS User (
+        UserId INTEGER PRIMARY KEY,
+        Reputation INTEGER,
+        CreationDate TEXT,
+        DisplayName TEXT,
+        LastAccessDate TEXT,
+        WebsiteUrl TEXT,
+        Location Text,
+        AboutMe Text,
+        Views INTEGER,
+        UpVotes INTEGER,
+        DownVotes INTEGER,
+        ProfileImageUrl TEXT,
+        AccountId INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS Post (
+        PostId                INTEGER PRIMARY KEY,
+        PostTypeId            INTEGER,
+        AcceptedAnswerId      INTEGER,
+        CreationDate          TEXT,
+        Score                 INTEGER,
+        ViewCount             INTEGER,
+        Body                  TEXT,
+        OwnerUserId           INTEGER,
+        LastEditorUserId      INTEGER,
+        LastEditorDisplayName TEXT,
+        LastEditDate          TEXT,
+        Title                 TEXT,
+        Tags                  TEXT,
+        AnswerCount           INTEGER,
+        CommentCount          INTEGER,
+        FavoriteCount         INTEGER,
+        CommunityOwnedDate    TEXT,
+        ContentLicense        TEXT,
+        ParentId              INTEGER,
+        FOREIGN KEY (OwnerUserId) REFERENCES User (UserId),
+        FOREIGN KEY (LastEditorUserId) REFERENCES User (UserId)
+);
+
+CREATE TABLE IF NOT EXISTS Comment (
+        CommentId INTEGER PRIMARY KEY,
+        PostId INTEGER,
+        Score INTEGER,
+        Body TEXT,
+        CreationDate TEXT,
+        UserId INTEGER,
+        ContentLicense TEXT,
+        FOREIGN KEY (PostId) REFERENCES Post (PostId),
+        FOREIGN KEY (UserId) REFERENCES User (UserId)
+
+);
+
+CREATE TABLE IF NOT EXISTS Badge (
+        BadgeId INTEGER PRIMARY KEY,
+        UserId INTEGER,
+        Name TEXT,
+        CreationDate TEXT,
+        Class INTEGER,
+        TagBased TEXT,
+        FOREIGN KEY (UserId) REFERENCES User (UserId)
+);
+
+
+CREATE TABLE IF NOT EXISTS Tag (
+        TagId INTEGER PRIMARY KEY,
+        TagName TEXT,
+        Count INTEGER
+);
+
+
+CREATE TABLE IF NOT EXISTS Vote (
+        VoteId INTEGER PRIMARY KEY,
+        PostId INTEGER,
+        VoteTypeId INTEGER,
+        CreationDate TEXT,
+        FOREIGN KEY (PostId) REFERENCES Post (PostId)
+);
