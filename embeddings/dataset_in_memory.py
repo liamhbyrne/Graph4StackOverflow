4490abfb96465d8903b30c8d259828676f9c0ecd
L.H.Byrne <lhb1g20@srv02441.soton.ac.uk>
2023-03-08 14:29:52 +0000

    working Hetero-GAT
    Former-commit-id: 2e4ad7391383ab0c21f7febaacd38ef5c00a543e

--- embeddings/dataset_in_memory.py
+++ embeddings/dataset_in_memory.py
@@ -1,5 +1,7 @@
 import logging
 import os
+import re
+from typing import List
 
 import torch
 from torch_geometric.data import InMemoryDataset
@@ -9,9 +11,12 @@ from custom_logger import setup_custom_logger
 log = setup_custom_logger('in-memory-dataset', logging.INFO)
 
 class UserGraphDatasetInMemory(InMemoryDataset):
-    def __init__(self, root, transform=None, pre_transform=None, pre_filter=None):
+    def __init__(self, root, file_name_out: str, question_ids:List[int]=None, transform=None, pre_transform=None, pre_filter=None):
+        self._file_name_out = file_name_out
+        self._question_ids = question_ids
         super().__init__(root, transform, pre_transform, pre_filter)
         self.data, self.slices = torch.load(self.processed_paths[0])
+        self.data = self.data.apply(lambda x: x.detach())
 
     @property
     def processed_dir(self):
@@ -27,7 +32,7 @@ class UserGraphDatasetInMemory(InMemoryDataset):
 
     @property
     def processed_file_names(self):
-        return ['in-memory-dataset.pt']
+        return [self._file_name_out]
 
     def download(self):
         pass
@@ -37,15 +42,34 @@ class UserGraphDatasetInMemory(InMemoryDataset):
         data_list = []
 
         for f in self.raw_file_names:
+            question_id_search = re.search(r"id_(\d+)", f)
+            if question_id_search:
+                if int(question_id_search.group(1)) not in self._question_ids:
+                    continue
+
             data = torch.load(os.path.join(self.raw_dir, f))
             data_list.append(data)
 
         data, slices = self.collate(data_list)
-        torch.save((data, slices), os.path.join(self.processed_dir, self.processed_file_names[0]))
+        self.processed_paths[0] = f"{len(data_list)}-{self.processed_file_names[0]}"
+        torch.save((data, slices), os.path.join(self.processed_paths[0]))
 
 
 
 if __name__ == '__main__':
-    dataset = UserGraphDatasetInMemory('../data/')
+    question_ids = set()
+    # Split by question ids
+    for f in os.listdir("../data/processed"):
+        question_id_search = re.search(r"id_(\d+)", f)
+        if question_id_search:
+            question_ids.add(int(question_id_search.group(1)))
 
-    print(dataset.get(3))
+    #question_ids = list(question_ids)[:len(question_ids)* 0.6]
+    train_ids = list(question_ids)[:int(len(question_ids) * 0.7)]
+    test_ids = [x for x in question_ids if x not in train_ids]
+
+    log.info(f"Training question count {len(train_ids)}")
+    log.info(f"Testing question count {len(test_ids)}")
+
+    train_dataset = UserGraphDatasetInMemory('../data/', train_ids, f'train-{len(train_ids)}-qs.pt')
+    test_dataset = UserGraphDatasetInMemory('../data/', test_ids, f'test-{len(test_ids)}-qs.pt')
