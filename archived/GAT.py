cdf02a296a0a6982d8a9b20f72e148a6edb85c62
Liam Byrne <liambyrneh@gmail.com>
2023-05-01 16:11:23 +0100

    restructure for first release

---
+++ archived/GAT.py
@@ -0,0 +1,101 @@
+import torch
+from torch_geometric.loader import DataLoader
+from torch_geometric.nn import GATConv, Linear, MeanAggregation, to_hetero
+
+from dataset import UserGraphDataset
+
+metadata = (['question', 'answer', 'comment', 'tag', 'module'],
+            [('tag', 'describes', 'question'), ('tag', 'describes', 'answer'), ('tag', 'describes', 'comment'), ('module', 'imported_in', 'question'), ('module', 'imported_in', 'answer'), ('question', 'rev_describes', 'tag'), ('answer', 'rev_describes', 'tag'), ('comment', 'rev_describes', 'tag'), ('question', 'rev_imported_in', 'module'), ('answer', 'rev_imported_in', 'module')])
+
+
+class Model(torch.nn.Module):
+    def __init__(self, hidden_channels):
+        super(Model, self).__init__()
+        self.lin = Linear(hidden_channels, 2)
+        self.softmax = torch.nn.Softmax(dim=1)
+        self.gat = to_hetero(GAT(hidden_channels), metadata)
+        self.pool = MeanAggregation()
+
+    def forward(self, data, post_emb):
+        convolved = self.gat(data.x_dict, data.edge_index_dict, data.batch_dict)
+        pooled = self.pool(convolved, data.batch_dict['question'])
+
+        x = torch.cat([convolved, post_emb], dim=1)
+
+        # 3. Concatenate with post embedding
+        # x = torch.cat((x, post_emb))
+        # 4. Apply a final classifier.
+        x = self.lin(x)
+        x = self.softmax(x)
+        return x
+class GAT(torch.nn.Module):
+    def __init__(self, hidden_channels):
+        super(GAT, self).__init__()
+        torch.manual_seed(12345)
+        self.conv1 = GATConv((-1, -1), hidden_channels, add_self_loops=False)
+        self.conv2 = GATConv((-1, -1), hidden_channels, add_self_loops=False)
+        self.conv3 = GATConv((-1, -1), hidden_channels, add_self_loops=False)
+
+
+
+    def forward(self, x, edge_index, batch):
+        # 1. Obtain node embeddings
+        x = self.conv1(x, edge_index)
+        x = x.relu()
+        x = self.conv2(x, edge_index)
+        x = x.relu()
+        x = self.conv3(x, edge_index)
+        x = x.relu()
+        # 2. Readout layer
+        x = self.pool(x, batch)  # [batch_size, hidden_channels]
+        return x
+
+
+
+def train(model, train_loader):
+    model.train()
+
+    for data in train_loader:  # Iterate in batches over the training dataset.
+        print(data)
+
+        out = model(data, torch.cat([data.question_emb, data.answer_emb], dim=1))  # Perform a single forward pass.
+        print(out, data.label)
+        loss = criterion(out, torch.squeeze(data.label, -1))  # Compute the loss.
+        loss.backward()  # Derive gradients.
+        optimizer.step()  # Update parameters based on gradients.
+        optimizer.zero_grad()  # Clear gradients.
+
+
+def test(loader):
+    model.eval()
+
+    correct = 0
+    for data in loader:  # Iterate in batches over the training/test dataset.
+        out = model(data.x, data.edge_index, data.batch)
+        pred = out.argmax(dim=1)  # Use the class with highest probability.
+        correct += int((pred == data.y).sum())  # Check against ground-truth labels.
+    return correct / len(loader.dataset)  # Derive ratio of correct predictions.
+
+
+if __name__ == '__main__':
+    dataset = UserGraphDataset(root="../data", skip_processing=True)
+    train_dataset, val_dataset, test_dataset = torch.utils.data.random_split(dataset, [0.6, 0.1, 0.3])
+
+    print(dataset.num_node_features)
+
+    train_loader = DataLoader(train_dataset, batch_size=1)
+    val_loader = DataLoader(val_dataset, batch_size=64)
+    test_loader = DataLoader(test_dataset, batch_size=64)
+
+    model = Model(hidden_channels=64)
+    sample = train_dataset[0]
+    #model = to_hetero(model, metadata)
+    #print(model(sample.x_dict, sample.edge_index_dict, sample.batch_dict))
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+    criterion = torch.nn.CrossEntropyLoss()
+
+    for epoch in range(1, 10):
+        train(model, train_loader)
+        #train_acc = test(train_loader)
+        #print(f'Epoch: {epoch:03d}, Train Acc: {train_acc:.4f}, Test Acc: {test_acc:.4f}')
---
+++ modules/models/helper_functions.py
@@ -0,0 +1,97 @@
+import networkx as nx
+import torch
+from torch_geometric.utils import to_networkx
+
+import wandb
+import os
+
+from Visualize import GraphVisualization
+from custom_logger import setup_custom_logger
+import logging
+
+log = setup_custom_logger("heterogenous_GAT_model", logging.INFO)
+
+
+def create_graph_vis(graph):
+    g = to_networkx(graph.to_homogeneous())
+    pos = nx.spring_layout(g)
+    vis = GraphVisualization(
+        g, pos, node_text_position='top left', node_size=20,
+    )
+    fig = vis.create_figure()
+    return fig
+
+
+"""
+Weights & Biases dashboard
+"""
+
+def init_wandb(project_name: str, dataset, data_details):
+    wandb.init(project=project_name, name="setup")
+
+    # Log all the details about the data to W&B.
+    wandb.log(data_details)
+
+    # Log exploratory visualizations for each data point to W&B
+    table = wandb.Table(columns=["Graph", "Number of Nodes", "Number of Edges", "Label"])
+    for graph in dataset:
+        fig = create_graph_vis(graph)
+        n_nodes = graph.num_nodes
+        n_edges = graph.num_edges
+        label = graph.label.item()
+
+        # graph_vis = plotly.io.to_html(fig, full_html=False)
+
+        table.add_data(wandb.Plotly(fig), n_nodes, n_edges, label)
+    wandb.log({"data": table})
+
+    # Log the dataset to W&B as an artifact.
+    dataset_artifact = wandb.Artifact(name="static-graphs", type="dataset", metadata=data_details)
+    dataset_artifact.add_dir("../data/")
+    wandb.log_artifact(dataset_artifact)
+
+    # End the W&B run
+    wandb.finish()
+
+def start_wandb_for_training(wandb_project_name: str, wandb_run_name: str):
+    wandb.init(project=wandb_project_name, name=wandb_run_name)
+    return wandb.config
+
+
+def add_cm_to_wandb(test_info):
+    wandb.log({"test/cm": wandb.plot.confusion_matrix(probs=None, y_true=test_info["trues"], preds=test_info["preds"], class_names=["neutral", "upvoted"])})
+    
+def log_results_to_wandb(results_map, results_name: str):
+    wandb.log({
+                f"{results_name}/loss": results_map["loss"],
+                f"{results_name}/accuracy": results_map["accuracy"],
+                f"{results_name}/f1-macro": results_map["f1-score-macro"],
+                f"{results_name}/f1-weighted": results_map["f1-score-weighted"],
+                f"{results_name}/table": results_map["table"]
+            })
+            
+"""
+PyTorch helpers
+"""
+
+
+def save_model(model, model_name: str):
+    torch.save(model.state_dict(), os.path.join("..", "models", model_name))
+
+
+def split_test_train_pytorch(dataset, train_split):
+    train_size = int(0.7 * len(dataset))
+    test_size = len(dataset) - (train_size)
+
+    train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
+    return train_dataset, test_dataset
+
+def calculate_class_weights(dataset):
+    # Class weights
+    log.info(f"Calculating class weights")
+    train_labels = [x.label for x in dataset]
+    counts = [train_labels.count(x) for x in [0, 1]]
+    class_weights = [1 - (x / sum(counts)) for x in counts]
+    log.info(class_weights)
+    
+    return torch.tensor(class_weights)
